857a7ee9af1072b8d73e5bcc7cc10573d5a5c043
Hirelane <nobilgautam007@gmail.com>
2024-01-24 12:49:54 +0530

    add item started

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
 function AddItem() {
+  //name image category shipping featured price description
   return (
-    <div>AddItem</div>
+    <>
+      <form>
+        <label>
+          <span>Product Name: </span>
+          <input />
+        </label>
+        <label>
+          <span>Category: </span>
+          <input />
+        </label>
+        <label>
+          <span>Description: </span>
+          <input />
+        </label>
+        <label>
+          <span>Price: </span>
+          <input />
+        </label>
+        <label>
+          <span>Product Name: </span>
+          <input />
+        </label>
+        <label>
+          <span>Product Name: </span>
+          <input />
+        </label>
+        <label>
+          <span>Product Name: </span>
+          <input />
+        </label>
+      </form>
+    </>
   )
 }
 
